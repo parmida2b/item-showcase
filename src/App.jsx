b5440e29a6da69ebf3a4205a91b3b04f0a5f2d3f
parmida2b <parmidamohamadzade@gmail.com>
2026-06-10 00:02:30 +0330

    codes has been modified

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const PRODUCTS_DATA = [
   {
     id: 1,
     name: "Iphone 14 promax 256 gig",
-    price: 93090,
+    price: 930.90,
     rating: 4.5,
     image: "./public/2d0d36367d682a77f57cd368a185a2f1c80ae2ce(1).png", 
     colors: ["#333", "#eee", "#748ba3"]
@@ -17,7 +17,7 @@ const PRODUCTS_DATA = [
   {
     id: 2,
     name: "Blackmagic Design Pocket Cinema Camera 6K Pro (Canon EF)",
-    price: 253500,
+    price: 2535.00,
     rating: 4.8,
     image: "./public/da0eb4b48976c397ec43f918ec3c69b360439260.png",
     colors: []
@@ -25,7 +25,7 @@ const PRODUCTS_DATA = [
     {
     id: 3,
     name: "SAMSUNG Galaxy S23 Ultra Cell Phone,256 GB",
-    price: 101800,
+    price: 1018.00,
     rating: 4.7,
     image: "./public/ffd4890b9f2cc8fe888e1faa56267ddc68972d3d.png",
     colors: ["#EADEE9", "#BCD7FF", "#8B8B8B"]
@@ -33,7 +33,7 @@ const PRODUCTS_DATA = [
       {
     id: 4,
     name: "VR VisionTech X1",
-    price: 139900,
+    price: 1399.00,
     rating: 3.9,
     image: "./public/2a32add338ec449147e77e734dbb17fc2a9946fa.png",
     colors: ["#B4B4B4"]
